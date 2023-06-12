869e3dea7ca163ed8710e84e877a06375edfc637
Arvind Sangwan <asangwan8998@gmail.com>
2023-06-12 16:11:03 -0400

    Added changes for typescript

--- src/components/name-badge.tsx
+++ src/components/name-badge.tsx
@@ -1,4 +1,15 @@
-const NameBadge = () => {
+const numberGetter = (n:number): number=>{
+return n+2;
+}
+const NameBadge = (
+  // {name} : {name:string}
+  props: {name: string}
+  )=> {
+  // : JSX.Element 
+  
+
+    if(Math.random()) return null;
+
   return (
     <section className="badge">
       <header className="badge-header">
@@ -6,11 +17,11 @@ const NameBadge = () => {
         <p>My name is…</p>
       </header>
       <div className="badge-body">
-        <p className="badge-name">Steve</p>
+        <p className="badge-name">{props.name}</p>
       </div>
       <footer className="badge-footer" />
     </section>
   );
 };
 
-export default NameBadge;
+export default NameBadge; 
